ce7573990c8b305106e7bd4dfa0e11f91886f752
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2022-04-10 23:07:00 -0300

    preparando o ambiente

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+
+try
+{
+    var uri = Environment.GetEnvironmentVariable("AMQP_URL") ?? "amqp://localhost";
+    var factory = new ConnectionFactory() { Uri = new Uri(uri) };
+    var connection = factory.CreateConnection();
+    System.Console.WriteLine("Conectado!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# tutorial-rabbitmq
+# .LaB
+
+## Ambiente
+
+- Definir a variável de ambiente:
+
+  `AMQP_URL=endereço do servidor AMQP`
+
+- Criar o projeto inicial
+
+  `dotnet new console -n lab-rabbitmq`
+
+- Adicionar pacote do RabbitMQ
+
+  `dotnet add package RabbitMQ.Client`
